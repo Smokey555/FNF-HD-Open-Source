275b6af6149636d574606b8465f7e2a433acf46a
y579 <84591776+y579@users.noreply.github.com>
2021-07-14 16:09:22 -0700

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -203,25 +203,7 @@ class Character extends FlxSprite
 				addOffset("singDOWN", -50, -130);
 
 				playAnim('danceRight');
-			case 'mom':
-				tex = Paths.getSparrowAtlas('Mom_Assets');
-				frames = tex;
-
-				animation.addByPrefix('idle', "Mom Idle", 24, false);
-				animation.addByPrefix('singUP', "Mom Up Pose", 24, false);
-				animation.addByPrefix('singDOWN', "MOM DOWN POSE", 24, false);
-				animation.addByPrefix('singLEFT', 'Mom Left Pose', 24, false);
-				// ANIMATION IS CALLED MOM LEFT POSE BUT ITS FOR THE RIGHT
-				// CUZ DAVE IS DUMB!
-				animation.addByPrefix('singRIGHT', 'Mom Pose Left', 24, false);
-
-				addOffset('idle');
-				addOffset("singUP", 14, 71);
-				addOffset("singRIGHT", 10, -60);
-				addOffset("singLEFT", 250, -23);
-				addOffset("singDOWN", 20, -160);
-
-				playAnim('idle');
+		//shes useless
 
 			case 'mom-car':
 				tex = Paths.getSparrowAtlas('momCar');
